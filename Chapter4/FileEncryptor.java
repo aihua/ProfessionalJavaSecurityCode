84bd5b7ab49c64d0887dd2a4a1dd9bb48f74cf18
bigdog001 <bigdog001@users.noreply.github.com>
2017-02-07 14:22:45 -0500

    Update FileEncryptor.java

--- Chapter4/FileEncryptor.java
+++ Chapter4/FileEncryptor.java
@@ -6,8 +6,16 @@ import java.io.*;
 /**
 
 解决java.security.InvalidKeyException Illegal key size问题
-1、到以下网址：http://www.oracle.com/technetwork/java/javase/downloads/index.html下载Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files 6得到包jce_policy-6.zip
-2、解决jce_policy-6.zip将里面的local_policy.jar和US_export_policy.jar复制并覆盖%JAVA_HOME%/jre/lib/security下的local_policy.jar和US_export_policy.jar
+如果密钥大于128, 会抛出java.security.InvalidKeyException: Illegal key size 异常. 因为密钥长度是受限制的, java运行时环境读到的是受限的policy文件. 文件位于${java_home}/jre/lib/security, 这种限制是因为美国对软件出口的控制.
+
+处理办法: 在官方网站下载JCE无限制权限策略文件
+
+JDK7的下载地址: http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html
+JDK8的下载地址: http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html 
+下载后解压，可以看到local_policy.jar和US_export_policy.jar以及readme.txt
+如果安装了JRE，将两个jar文件放到%JRE_HOME%\lib\security目录下覆盖原来的文件
+如果安装了JDK，还要将两个jar文件也放到%JDK_HOME%\jre\lib\security目录下覆盖原来文件
+
 
  *	This class encrypts and decrypts a file using CipherStreams
  *	and a 256-bit Rijndael key stored in the filesystem.
